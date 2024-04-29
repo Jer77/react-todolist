46f40fecf7064e5eb167e300bb80a4211d4653f9
Jer77 <36992332+Jer77@users.noreply.github.com>
2024-04-29 11:44:26 -0500

    src adds

---
+++ src/components/todoApp.js
@@ -0,0 +1,8 @@
+export default function TodoApp() {
+    return <div className="todoContainer">
+        <form className="todoCreateForm">
+            <input className="todoInput"></input>
+            <input type="submit" value="Create to do"></input>
+        </form>
+    </div>
+}
