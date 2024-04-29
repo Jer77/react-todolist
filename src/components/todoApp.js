318d31603c3aa7bbdd2ac760b27bb1aa6a803505
Jer77 <rmjeracm@gmail.com>
2024-04-29 14:15:32 -0500

    added event on submit

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -3,11 +3,12 @@ import { useState } from "react";
 export default function TodoApp() {
 
     const [title, setTilte] = useState("");
+    const [toDos, setToDos] = useState([]);
 
-    function handleClick(e) {
+    /* function handleClick(e) {
         e.preventDefault();
         setTilte("Texto");
-    }
+    } */
 
     function handleChange(e) {
         const value = e.target.value;
@@ -15,10 +16,25 @@ export default function TodoApp() {
         setTilte(value);
     }
 
+    function handleSubmit (e) {
+        e.preventDefault();
+
+        const newToDo = {
+            id: crypto.randomUUID(),
+            title: title,
+            completed: false
+        }
+
+        const toDoTemp = [ ... toDos];
+        toDoTemp.unshift(newToDo);
+
+        setToDos(toDoTemp);
+    }
+
     return <div className="todoContainer">
-        <form className="todoCreateForm">
+        <form className="todoCreateForm"onSubmit={ handleSubmit }>
             <input onChange={ handleChange } className="todoInput" value={ title }></input>
-            <input onClick={ handleClick }
+            <input onClick={ handleSubmit }
                 type="submit"
                 value="Create to do"
                 className="buttonCreate">
